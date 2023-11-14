fb67296752887965671b29de936e4e87536a2a2d
Nikola Perić <np54387@fer.hr>
2023-11-14 17:34:32 +0100

    Prepravljen kod za Login i App te dodan redirect na Home page

--- IzvorniKod/error404-fe/error404/src/App.tsx
+++ IzvorniKod/error404-fe/error404/src/App.tsx
@@ -1,53 +1,17 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
-
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
 
 function App() {
-
-  const handleLogin = (username: string, password: string) => {
-
-    const correctLogin = [['Nikola','12345'] , ['Ivo','lozinka']]; // ovo je testno da mozemo otic na glavnu stranicu
-
-    function isLoginCorrect(user: string, pass: string) {
-      for (var i = 0; i < correctLogin.length; i++) {
-        var pair = correctLogin[i];
-        if (pair[0] === user && pair[1] === pass) {
-          return true; // Found a match
-        }
-      }
-      return false; // No match found
-    }
-
-    if (isLoginCorrect(username,password)){
-      console.log("Password is correct")
-      console.log(username)
-      console.log(password)
-      return <Navigate to="/home" />;
-
-      // ovo bi ga trebalo redirectat na main stranicu
-
-    } else {
-      console.log("Try again")
-      // ovo treba izbacit alert da je netocno
-    }
-
-    // implementiraj pravu provjeru logina
-
-  }
-
-
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path='/' element = {<Login onLogin={handleLogin} />}></Route>
-          <Route path='/home' element = {<Home />}></Route>
+          <Route path='/' element={<Login />} />
+          <Route path='/home' element={<Home />} />
         </Routes>
       </div>
     </BrowserRouter>
-
   );
 }
 
--- IzvorniKod/error404-fe/error404/src/components/Login.tsx
+++ IzvorniKod/error404-fe/error404/src/components/Login.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Login.css';
 
+const correctLogin = [['Nikola','12345'] , ['Ivo','lozinka']];
 
-interface Props{
-    onLogin: (username: string, password: string) => void;
-}
-
-function Login({onLogin}: Props){
+function Login(){
+  const navigate = useNavigate();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    onLogin(username,password);
+  function isLoginCorrect(user: string, pass: string) {
+    for (var i = 0; i < correctLogin.length; i++) {
+      var pair = correctLogin[i];
+      if (pair[0] === user && pair[1] === pass) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  const handleLogin = (event: FormEvent) => {
+
+    event.preventDefault(); // da ne bi refreshalo
+
+    if (isLoginCorrect(username,password)){
+      console.log("Password is correct");
+      navigate('/home'); // redirecta na home ako su podatci dobri
+
+    } else {
+      alert('Incorrect username or password. Try again.'); //obacijesti da je netocno
+
+    }
   };
 
   return (
+
     <div className="login-container" style={{ backgroundColor: '#f0f8ff' }}>
-        <form className="login-box">
+        <form className="login-box" onSubmit={handleLogin}>
             <h2 className="text-center mb-3"> Login</h2>
+            
             <div className="mb-3">
                 <label htmlFor="loginUsername" className="form-label">Username</label>
                 <input type="text" className="form-control" id="loginUsername" aria-describedby="emailHelp"
                 value={username} onChange={(e) => setUsername(e.target.value)} />
                 <div id="emailHelp" className="form-text">Input your admin credentials </div>
             </div>
+
             <div className="mb-3">
                 <label htmlFor="loginPassword" className="form-label">Password</label>
                 <input type="password" className="form-control" id="loginPassword" 
                 value={password} onChange={(e) => setPassword(e.target.value)} />
             </div>
-            <button type="submit" className="btn btn-primary" onClick={handleLogin}>Submit</button>
+
+            <button type="submit" className="btn btn-primary">Submit</button>
         </form>
     </div>
   );
