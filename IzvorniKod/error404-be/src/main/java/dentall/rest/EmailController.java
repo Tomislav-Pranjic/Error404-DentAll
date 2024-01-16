218ed5c557c573e0dc9103f13cbc1e053a5c12b9
jmX86 <jm54307@fer.hr>
2024-01-16 16:45:39 +0100

    Zakomentirao mail dio aplikacije zato što je bez valjanih korisničkih podataka rušio aplikaciju na Renderu

--- IzvorniKod/error404-be/src/main/java/dentall/rest/EmailController.java
+++ IzvorniKod/error404-be/src/main/java/dentall/rest/EmailController.java
@@ -3,6 +3,7 @@ package dentall.rest;
 import dentall.rest.dto.CreateEmailDTO;
 import dentall.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Profile("email")
 @RequestMapping("/email")
 @PreAuthorize("hasRole('ROLE_SMJESTAJNI') or hasRole('ROLE_KORISNICKI') or hasRole('ROLE_PRIJEVOZNI')")
 public class EmailController {
