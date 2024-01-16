0d557125dc87408917df77d35a97ed03562789f2
jmX86 <jm54307@fer.hr>
2024-01-16 15:57:12 +0100

    Prebacio /login checkpoint u /login/checkCredentials

--- IzvorniKod/error404-be/src/main/java/dentall/rest/LoginController.java
+++ IzvorniKod/error404-be/src/main/java/dentall/rest/LoginController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LoginController {
-    @PostMapping("/login")
+    @PostMapping("/login/checkCredentials")
     public void login() {
     }
 }
