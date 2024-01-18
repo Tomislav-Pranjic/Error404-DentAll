c79ffa2155d5f9159eea47ce0d5929866e890e4c
nperitzch <np54387@fer.hr>
2024-01-18 23:51:57 +0100

    pripremljen TRansportation.tsx za dodavanje i editanje vozaca, no fecth baca error

--- IzvorniKod/error404-fe/src/components/Driver.tsx
+++ IzvorniKod/error404-fe/src/components/Driver.tsx
@@ -1,28 +1,12 @@
 
-import { useState } from 'react';
-
-
 function Driver(props: { driver: { driverId: any; name: any; surname: any; email: any; phoneNumber: any; vehicle: any; workStartTime: any; workingDays: any; }; }) {
-    const [showMore, setShowMore] = useState(false);
-    const {driverId,name,surname,email,phoneNumber,vehicle,workStartTime,workingDays} = props.driver;
 
-    const handleToggleShowMore = () => {
-        setShowMore(!showMore);
-    };
+    const {driverId,name,surname,email,phoneNumber,vehicle,workStartTime,workingDays} = props.driver;
 
     return (
         <div className="driver-card">
           <div>
             <h2>{name} {surname}</h2>
-            {showMore && (
-              <div>
-                <p>Email: {email}</p>
-                <p>Phone Number: {phoneNumber}</p>
-                {/* <p>Vehicle Registration: {vehicleReg}</p> */}
-                <p>Work Start Time: {workStartTime}</p>
-                <p>Working Days: {workingDays}</p>
-              </div>
-            )}
           </div>
         </div>
         
--- IzvorniKod/error404-fe/src/components/Transportation.tsx
+++ IzvorniKod/error404-fe/src/components/Transportation.tsx
@@ -8,8 +8,17 @@ import { Base64 } from 'js-base64';
 function Transportation(){
 
   const[drivers,setDrivers] = useState<any[]>([]);
+  const[vehicles,setVehicles] = useState<any[]>([]);
+  const[toggleAddDriver,setToggleAddDriver] = useState(false);
+
   const [editDriver, setEditDriver] = useState<number | null>(null);
-  const [driverForm, setDriverForm] = useState({ name: '', surname: '',email: '',phoneNumber: '',workStartTime: '',workingDays: ''});
+  const [driverForm, setDriverForm] = useState({ name: '', surname: '',email: '',phoneNumber: '',
+  vehicle: {
+    registration: '',
+    model: '',
+    color: '',
+    capacity: 0
+  },workStartTime: '',workingDays: ''});
 
   const storedUsername = localStorage.getItem('username');
   const storedPassword = localStorage.getItem('password');
@@ -32,14 +41,19 @@ function Transportation(){
     fetch('/api/driver')
       .then(data => data.json())
       .then(drivers => setDrivers(drivers))
-
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      })
   },[]);
+  
+  const handleAddDriverForm= () => {
+    setToggleAddDriver(!toggleAddDriver);
+  };
 
   function onChange(event: React.ChangeEvent<HTMLInputElement>) {
     const {name, value} = event.target;
     setDriverForm(oldForm => ({...oldForm, [name]: value}))
   }
-
 
   const handleToggleShowMore = (driverId: number) => {
     setEditDriver(prevId => (prevId === driverId ? null : driverId));
@@ -47,18 +61,141 @@ function Transportation(){
 
   const handleEdit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    console.log("edit")
-  };
+
+    // const optionsVehicleGET = {
+    //   method: 'GET',
+    //   headers: new Headers({
+    //       "Authorization": `Basic ${Base64.encode(`${storedUsername}:${storedPassword}`)}`,
+    //       "Content-Type": "application/json",
+    //         "Accept": "*/*",
+    //       "Accept-Encoding": "gzip, deflate, br",
+    //       "Connection": "keep-alive"
+    //   }),
+    //   body: ''
+    // };
+    // fetch('/api/vehicle')
+    //   .then(data => data.json())
+    //   .then(vehicles => setVehicles(vehicles))
+
+
+    const dataVehicle = {
+      registration: driverForm.vehicle.registration,
+      model: driverForm.vehicle.model,
+      color: driverForm.vehicle.color,
+      capacity: driverForm.vehicle.capacity
+    }
+
+
+    const optionsVehiclePatch = {
+      method: 'PATCH',
+      headers: new Headers({
+          "Authorization": `Basic ${Base64.encode(`${storedUsername}:${storedPassword}`)}`,
+          "Content-Type": "application/json",
+            "Accept": "*/*",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Connection": "keep-alive"
+      }),
+      body: JSON.stringify(dataVehicle)
+    };
+    fetch('api/vehicle', optionsVehiclePatch)
+    .then(response => {
+      if(response.ok){
+        console.log('Uspjesno editano vozilo')
+      }
+    })
+
+    const dataDriver= {
+      firstName: driverForm.name, 
+      lastName: driverForm.surname,
+      email: driverForm.email,
+      phoneNumber: driverForm.phoneNumber,
+      vehicleReg: driverForm.vehicle.registration,
+      workStartTime: driverForm.workStartTime,
+      workingDays: driverForm.workingDays
+    }
+    const optionsDriver = {
+      method: 'PATCH',
+      headers: new Headers({
+          "Authorization": `Basic ${Base64.encode(`${storedUsername}:${storedPassword}`)}`,
+          "Content-Type": "application/json",
+            "Accept": "*/*",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Connection": "keep-alive"
+      }),
+      body: JSON.stringify(dataDriver)
+    };
+    fetch('/api/driver',optionsDriver)
+    .then(response => {
+      if(response.ok){
+        console.log('Uspjesno editan vozac')
+      }
+    })
+  }
+
+
+
 
 
   const handleDelete = (id: number) => {
     console.log(`Delete button clicked for user with ID ${id}`);
   };
 
-  const handleAdd = () => {
-    console.log("Add button clicked");
+  const handleAdd = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const dataVehicle = {
+      registration: driverForm.vehicle.registration,
+      model: driverForm.vehicle.model,
+      color: driverForm.vehicle.color,
+      capacity: driverForm.vehicle.capacity
+    }
+    const dataDriver= {
+      firstName: driverForm.name, 
+      lastName: driverForm.surname,
+      email: driverForm.email,
+      phoneNumber: driverForm.phoneNumber,
+      vehicleReg: driverForm.vehicle.registration,
+      workStartTime: driverForm.workStartTime,
+      workingDays: driverForm.workingDays
+    }
+
+    const optionsVehiclePost = {
+      method: 'POST',
+      headers: new Headers({
+          "Authorization": `Basic ${Base64.encode(`${storedUsername}:${storedPassword}`)}`,
+          "Content-Type": "application/json",
+            "Accept": "*/*",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Connection": "keep-alive"
+      }),
+      body: JSON.stringify(dataVehicle)
+    };
+    fetch('api/vehicle', optionsVehiclePost)
+    .then(response => {
+      if(response.ok){
+        console.log('Uspjesno dodano vozilo')
+      }
+    })
+    const optionsDriver = {
+      method: 'PATCH',
+      headers: new Headers({
+          "Authorization": `Basic ${Base64.encode(`${storedUsername}:${storedPassword}`)}`,
+          "Content-Type": "application/json",
+            "Accept": "*/*",
+          "Accept-Encoding": "gzip, deflate, br",
+          "Connection": "keep-alive"
+      }),
+      body: JSON.stringify(dataDriver)
+    };
+    fetch('/api/driver',optionsDriver)
+    .then(response => {
+      if(response.ok){
+        console.log('Uspjesno dodan vozac')
+      }
+    })
   };
- 
+
+
 
 
 
@@ -129,7 +266,6 @@ function Transportation(){
                             onChange={onChange}
                           />
                         </label>
-                        <br />
                         <label className="form-label">
                           Last Name:
                           <input
@@ -145,19 +281,38 @@ function Transportation(){
                           Email:
                           <input className="form-control" type="text" name='email' defaultValue={driver.email} onChange={onChange}/>
                         </label>
-                        <br />
+
                         <label className="form-label">
                           Phone Number:
                           <input className="form-control" type="text" name='phoneNumber' defaultValue={driver.phoneNumber} onChange={onChange}/>
                         </label>
+                        
                         <br />
                         <label className="form-label">
                           Start of Work Time:
-                          <input className="form-control" type="text" name='workStartTime' defaultValue={driver.workStartTime} onChange={onChange}/>
+                          <input className="form-control" type="time" name='workStartTime' defaultValue={driver.workStartTime} onChange={onChange}/>
                         </label>
                         <label className="form-label">
                           Working Days:
                           <input className="form-control" type="text" name='workingDays' defaultValue={driver.workingDays} onChange={onChange}/>
+                        </label>
+                        <br />
+                        <label className='form-label'>
+                          Vehicle Registration:
+                          <input className='form-control' type="text" name='vehicle.registration' defaultValue={driver.vehicle.registration} onChange={onChange}/>
+                        </label>
+                        <label className='form-label'>
+                          Vehicle Model:
+                          <input className='form-control' type="text" name='vehicle.model' defaultValue={driver.vehicle.model} onChange={onChange}/>
+                        </label>
+                        <br />
+                        <label className='form-label'>
+                          Vehicle Color:
+                          <input className='form-control' type="text" name='vehicle.color' defaultValue={driver.vehicle.color} onChange={onChange}/>
+                        </label>
+                        <label className='form-label'>
+                          Vehicle Capacity:
+                          <input className='form-control' type="number" name='vehicle.capacity' defaultValue={driver.vehicle.capacity} onChange={onChange}/>
                         </label>
                         <br />
                         <button type="submit" className="btn btn-primary">Save</button>
@@ -169,6 +324,77 @@ function Transportation(){
               ))}
             </tbody>
           </table>
+          {toggleAddDriver && (
+            <form className="login-box" onSubmit={handleAdd}>
+            <label className="form-label">
+              First Name:
+              <input
+                className="form-control"
+                type="text"
+                name="name"
+                placeholder='Name'
+                onChange={onChange}
+              />
+            </label>
+            <label className="form-label">
+              Last Name:
+              <input
+                className="form-control"
+                type="text"
+                name='surname'
+                placeholder='Surname'
+                onChange={onChange}
+              />
+            </label>
+            <br />
+            <label className="form-label">
+              Email:
+              <input className="form-control" type="text" name='email' placeholder='namesurname@gmail.com' onChange={onChange}/>
+            </label>
+
+            <label className="form-label">
+              Phone Number:
+              <input className="form-control" type="text" name='phoneNumber' placeholder='0999876543' onChange={onChange}/>
+            </label>
+            
+            <br />
+            <label className="form-label">
+              Start of Work Time:
+              <input className="form-control" type="time" name='workStartTime' placeholder='HH:mm' onChange={onChange}/>
+            </label>
+            <label className="form-label">
+              Working Days:
+              <input className="form-control" type="text" name='workingDays' placeholder="NPUSCEB" onChange={onChange}/>
+            </label>
+            <br />
+            <label className='form-label'>
+              Vehicle Registration:
+              <input className='form-control' type="text" name='vehicle.registration' placeholder='AZ1234AZ' onChange={onChange}/>
+            </label>
+            <label className='form-label'>
+              Vehicle Model:
+              <input className='form-control' type="text" name='vehicle.model' placeholder='Model' onChange={onChange}/>
+            </label>
+            <br />
+            <label className='form-label'>
+              Vehicle Color:
+              <input className='form-control' type="text" name='vehicle.color' placeholder='Color' onChange={onChange}/>
+            </label>
+            <label className='form-label'>
+              Vehicle Capacity:
+              <input className='form-control' type="number" name='vehicle.capacity' placeholder='Capacity' onChange={onChange}/>
+            </label>
+            <br />
+            <button type="submit" className="btn btn-primary me-5 px-3" >Save</button>
+            <button type='button' className='btn btn-danger px-3' onClick={handleAddDriverForm}>Cancel</button>
+          </form>
+          )}
+
+          {!toggleAddDriver && (
+            <button type='button' className='btn btn-secondary ' onClick={handleAddDriverForm}>
+            Add Driver
+            </button>
+          )}
         </div>
       </main>
 
@@ -177,17 +403,6 @@ function Transportation(){
       </footer>
     </div>
   );
-}
+};
 
 export default Transportation;
-
-
-// {showMore && (
-//   <div>
-//     <p>Email: {email}</p>
-//     <p>Phone Number: {phoneNumber}</p>
-//     {/* <p>Vehicle Registration: {vehicleReg}</p> */}
-//     <p>Work Start Time: {workStartTime}</p>
-//     <p>Working Days: {workingDays}</p>
-//   </div>
-// )}
